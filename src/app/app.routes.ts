b465f3629f7e40328ed2da45d542bf36ed1cb406
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-25 10:52:23 +0000

    login and signup components routed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'signup',
+        title: 'Frontend Mentor | Signup',
+        component: SignupComponent
+    },
+    {
+        path: '',
+        title: 'Frontend Mentor | Login',
+        component: LoginComponent
+    }
+];
