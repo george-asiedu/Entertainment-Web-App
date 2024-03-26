88de1a08ccc27c86090cf4b9a6acff106897fbec
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-26 11:18:28 +0000

    login component ts updated

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Login } from '../../model/netflix';
+import { NetflixService } from '../../service/netflix.service';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, FontAwesomeModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  credentials: Login = { email: '', password: '' }
+  error: string = ''
+  isLoggedIn: boolean = false
+  faSquareCheck = faSquareCheck
+  faCircleXmark = faCircleXmark
 
+  constructor(private netflixService: NetflixService, private router: Router) {}
+
+  login() {
+    this.netflixService.userLogin(this.credentials).subscribe(
+      (response) => {
+        console.log('Login Successful: ', response)
+        this.isLoggedIn = true
+        this.router.navigate(['/home'])
+      }, 
+      (error) => {
+        console.error("Login Error: ", error)
+        this.error = 'Invalid email or password'
+        this.isLoggedIn = false
+      }
+    )
+  }
 }
