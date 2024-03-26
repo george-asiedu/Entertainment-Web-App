022d0eb0d9ffe3b5f61819d8ccde2c5075d4b0bc
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-26 15:47:09 +0000

    navigate to home component

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+  constructor(private router: Router) {}
+
+  logout(): void {
+    this.router.navigate([''])
+  }
+}
