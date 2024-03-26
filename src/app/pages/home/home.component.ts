6734b53eefa1bec242acf40b1562305c6a23793b
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-26 16:00:06 +0000

    search functionality implemented and movie list items fetched from the service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { Netflix } from '../../model/netflix';
+import { NetflixService } from '../../service/netflix.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  movieItems: Netflix[] = []
+  search: any;
 
+  constructor(private netflixService: NetflixService) {}
+
+  ngOnInit(): void {
+      this.netflixService.getMovies().subscribe(
+        (response) => {
+          this.movieItems = response
+        }
+      )
+  }
+
+  searchlist(): void {
+    if(this.search === '') {
+      this.ngOnInit()
+    } else {
+      this.movieItems = this.movieItems.filter((item) => {
+        return item.title.toLowerCase().match(this.search.toLowerCase())
+      })
+    }
+  }
 }
