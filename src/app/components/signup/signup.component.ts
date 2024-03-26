88457ff19e13fcd7530993aa5ec524e7e3b4310f
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-26 11:19:08 +0000

    signup component ts updated

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Signup } from '../../model/netflix';
+import { NetflixService } from '../../service/netflix.service';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, FontAwesomeModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  user: Signup = { email: '', password: '', repeatPassword: ''}
+  passwordMatch: boolean = true
+  isSignedIn: boolean = false
+  faSquareCheck = faSquareCheck
+  faCircleXmark = faCircleXmark
 
+  constructor(private netflixService: NetflixService, private router: Router) {}
+
+  signUp() {
+    if(this.passwordMatch) {
+      this.netflixService.postUsers(this.user).subscribe(
+        (response) => {
+          console.log('Signup Successful: ', response)
+          this.isSignedIn = true
+          this.router.navigate([''])
+        }, 
+        (error) => {
+          console.error("Signup Error: ", error)
+          this.isSignedIn = false
+        }
+      )
+    } else {
+      console.error('Passwords do not match')
+    }
+  }
+
+  checkPassword() {
+    this.passwordMatch = this.user.password === this.user.repeatPassword
+  }
 }
