7464e4e59f2b62bfe1bf0ac1e2ef9585549fe9c8
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-26 11:17:22 +0000

    app.config.ts file updated to use the hppt client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { NetflixService } from './service/netflix.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
 };
