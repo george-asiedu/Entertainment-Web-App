7514ea847d0169f9b256501d0382fc3b5da8fdfc
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-26 15:46:24 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SignupComponent } from './components/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SignupComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
