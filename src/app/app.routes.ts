6516d09b61831a72b13a209f6143589307794dd1
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-27 14:07:43 +0000

    series route enabled

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,27 +3,33 @@ import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
+import { SeriesComponent } from './pages/series/series.component';
 
 
 export const routes: Routes = [
     {
         path: 'signup',
-        title: 'Frontend Mentor | Signup',
+        title: 'Frontend Mentor | Entertainment Web App - Signup',
         component: SignupComponent
     },
     {
         path: '',
-        title: 'Frontend Mentor | Login',
+        title: 'Frontend Mentor | Entertainment Web App - Login',
         component: LoginComponent
     },
     {
         path: 'home',
-        title: 'Frontend Mentor | Home',
+        title: 'Frontend Mentor | Entertainment Web App - Home',
         component: HomeComponent
     },
     {
         path: 'movies',
-        title: 'Frontend Mentor | Movies',
+        title: 'Frontend Mentor | Entertainment Web App - Movies',
         component: MoviesComponent
+    },
+    {
+        path: 'series',
+        title: 'Frontend Mentor | Entertainment Web App - TV Series',
+        component: SeriesComponent
     }
 ];
