bd8e61b9e73403820ad244fbccf93acbe8660b36
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-27 13:58:31 +0000

    component ts updated to filter only movies categories from the response

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { Netflix } from '../../model/netflix';
+import { NetflixService } from '../../service/netflix.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, FormsModule],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss'
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit{
+  movies: Netflix[] = []
+  search: string = ''
+  searchResultCount: number = 0
 
+  constructor(private ns: NetflixService) {}
+
+  ngOnInit(): void {
+      this.ns.getMovies().subscribe(
+        (movie) => {
+          this.movies = movie.filter(mov => mov.category === 'Movie')
+        }
+      )
+  }
+
+  searchlist(): void {
+    if(this.search === '') {
+      this.ngOnInit()
+    } else {
+        const searchTerm = this.search.toLowerCase()
+        this.movies = this.movies.filter((item) => {
+        return item.title.toLowerCase().includes(searchTerm)
+      })
+      this.searchResultCount = this.movies.length
+    }
+  }
 }
