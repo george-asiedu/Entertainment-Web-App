abd2f3146ba81b50f063d28b9153ab18e3356831
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-27 13:33:03 +0000

    movies component route enabled

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
 
 export const routes: Routes = [
@@ -19,5 +20,10 @@ export const routes: Routes = [
         path: 'home',
         title: 'Frontend Mentor | Home',
         component: HomeComponent
+    },
+    {
+        path: 'movies',
+        title: 'Frontend Mentor | Movies',
+        component: MoviesComponent
     }
 ];
